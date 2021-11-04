8d5d74857d8a9d623ac95c13a4930fd664f93aa9
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-03 18:28:19 -0600

    geenerate page function and new js file

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    console.log('==============');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-    // 
-    profileDataArr.forEach(profileItem => console.log(profileItem));;
-};
 
-printProfileData(profileDataArgs);
+
+
+//   console.log('Ben', 'Plaindemon');
+//   console.log(generatePage('Ben', 'Plaindemon'));
+
+// fs is defined using a built in node  tool
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw  new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
