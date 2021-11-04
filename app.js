47dd5eafd0a8a192071cc9364472e02fd6982be9
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-03 18:50:52 -0600

    commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-//   console.log('Ben', 'Plaindemon');
-//   console.log(generatePage('Ben', 'Plaindemon'));
-
-// fs is defined using a built in node  tool
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw  new Error(err);
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
